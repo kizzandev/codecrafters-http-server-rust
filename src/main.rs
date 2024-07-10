f2e1be3d3da5e3eccc023207e9db8ad9d3c8b566
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-10 10:09:13 -0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::net::TcpListener;
 use std::io::Write;
 
-struct Response {
-    status: String,
-    headers: String,
-    body: String,
-}
+// struct Response {
+//     status: String,
+//     headers: String,
+//     body: String,
+// }
 
-struct Request {
-    method: String,
-    uri: String,
-    version: String,
-    headers: String,
-    body: String,
-}
+// struct Request {
+//     method: String,
+//     uri: String,
+//     version: String,
+//     headers: String,
+//     body: String,
+// }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -25,13 +25,13 @@ fn main() {
 
 
                 // Extract METHOD, URI, HTTP version, headers, and body
-                let mut request = Request {
-                    method: String::from(""),
-                    uri: String::from(""),
-                    version: String::from(""),
-                    headers: String::from(""),
-                    body: String::from(""),
-                };
+                // let mut request = Request {
+                //     method: String::from(""),
+                //     uri: String::from(""),
+                //     version: String::from(""),
+                //     headers: String::from(""),
+                //     body: String::from(""),
+                // };
 
                 // Extract method from _stream
                 eprintln!("The _stream is:\n{:?}", _stream);
