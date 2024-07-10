bfabca04fee53966d689a056475f0f29e5e7467b
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-10 10:20:40 -0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,41 @@ use std::io::Read;
 //     body: String,
 // }
 
-// struct Request {
-//     method: String,
-//     uri: String,
-//     version: String,
-//     headers: String,
-//     body: String,
-// }
+struct Request {
+    method: String,
+    uri: String,
+    version: String,
+    headers: String,
+    body: String,
+}
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    eprintln!("request: {}", String::from_utf8_lossy(&buffer));
+    let request_str = String::from_utf8_lossy(&buffer);
+    eprintln!("request: {}", request_str);
 
-    let _ = stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
-
+    // The string must start with a method (e.g. GET)
+    let method = request_str.lines().next().unwrap().split(' ').collect::<Vec<&str>>()[0];
+    let uri = request_str.lines().next().unwrap().split(' ').collect::<Vec<&str>>()[1];
+    let version = request_str.lines().next().unwrap().split(' ').collect::<Vec<&str>>()[2];
+    let headers = request_str.lines().skip(1).collect::<Vec<&str>>().join("\n");
+    let body = request_str.lines().skip(2).collect::<Vec<&str>>().join("\n");
     
-    // let mut request = Request {
-    //     method: String::from(""),
-    //     uri: String::from(""),
-    //     version: String::from(""),
-    //     headers: String::from(""),
-    //     body: String::from(""),
-    // };
+    let request = Request {
+        method: String::from(method),
+        uri: String::from(uri),
+        version: String::from(version),
+        headers: String::from(headers),
+        body: String::from(body),
+    };
 
+    eprintln!("method: {}", request.method);
+    eprintln!("uri: {}", request.uri);
+    eprintln!("version: {}", request.version);
+    eprintln!("headers: {}", request.headers);
+    eprintln!("body: {}", request.body);
+    
     // let status = match request.uri.as_str() {
     //     Some(uri) => {
     //         "HTTP/1.1 200 OK\r\n\r\n"
@@ -41,8 +52,9 @@ fn handle_connection(mut stream: TcpStream) {
     //         "HTTP/1.1 404 Not Found\r\n\r\n"
     //     }
     // }
+    let _ = stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
 }
-
+            
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
