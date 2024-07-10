e2583691bd8abc4e0cce02cc09213afb3607f1d2
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-10 10:07:12 -0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::net::TcpListener;
 use std::io::Write;
 
+struct Response {
+    status: String,
+    headers: String,
+    body: String,
+}
+
+struct Request {
+    method: String,
+    uri: String,
+    version: String,
+    headers: String,
+    body: String,
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
@@ -9,7 +23,29 @@ fn main() {
             Ok(mut _stream) => {
                 eprintln!("accepted new connection");
 
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+                // _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+
+                // Extract METHOD, URI, HTTP version, headers, and body
+                let mut request = Request {
+                    method: String::from(""),
+                    uri: String::from(""),
+                    version: String::from(""),
+                    headers: String::from(""),
+                    body: String::from(""),
+                };
+
+                // Extract method from _stream
+                eprintln("The _stream is:\n{:?}", _stream);
+
+
+                // let status = match request.uri.as_str() {
+                //     Some(uri) => {
+                //         "HTTP/1.1 200 OK\r\n\r\n"
+                //     },
+                //     None => {
+                //         "HTTP/1.1 404 Not Found\r\n\r\n"
+                //     }
+                // }
             }
             Err(e) => {
                 eprintln!("error: {}", e);
