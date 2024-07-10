6f1369e733438d0d7e72712e09cdfca68daebba6
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-10 10:15:02 -0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,32 @@ use std::io::Write;
 //     body: String,
 // }
 
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    eprintln!("request: {}", String::from_utf8_lossy(&buffer));
+
+    let _ = _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
+
+    
+    // let mut request = Request {
+    //     method: String::from(""),
+    //     uri: String::from(""),
+    //     version: String::from(""),
+    //     headers: String::from(""),
+    //     body: String::from(""),
+    // };
+
+    // let status = match request.uri.as_str() {
+    //     Some(uri) => {
+    //         "HTTP/1.1 200 OK\r\n\r\n"
+    //     },
+    //     None => {
+    //         "HTTP/1.1 404 Not Found\r\n\r\n"
+    //     }
+    // }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
@@ -22,32 +48,7 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 eprintln!("accepted new connection");
-
-
-                // Extract METHOD, URI, HTTP version, headers, and body
-                // let mut request = Request {
-                //     method: String::from(""),
-                //     uri: String::from(""),
-                //     version: String::from(""),
-                //     headers: String::from(""),
-                //     body: String::from(""),
-                // };
-
-                // Extract method from _stream
-                eprintln!("The _stream is:\n{:?}", _stream);
-
-
-                // let status = match request.uri.as_str() {
-                //     Some(uri) => {
-                //         "HTTP/1.1 200 OK\r\n\r\n"
-                //     },
-                //     None => {
-                //         "HTTP/1.1 404 Not Found\r\n\r\n"
-                //     }
-                // }
-
-                // let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
-                let _ = _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
+                handle_connection(_stream);
             }
             Err(e) => {
                 eprintln!("error: {}", e);
