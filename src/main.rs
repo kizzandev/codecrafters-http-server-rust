60c01d4f9766d9c1b0f7e98950162c72e50ac41b
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-10 21:05:16 -0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -199,9 +199,9 @@ fn handle_connection(mut stream: TcpStream) {
     );
     let mut response_bytes = response_str.as_bytes();
     if is_encoded {
-        response_bytes.extend_from_slice(&compressed);
+        response_bytes.extend(&compressed);
     } else {
-        response_bytes.extend_from_slice(response.body.as_bytes());
+        response_bytes.extend(response.body.as_bytes());
     }
     let _ = stream.write_all(&response_bytes).expect("Failed to write to stream");;
 }
