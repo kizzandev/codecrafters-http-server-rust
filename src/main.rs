8ba90e2eac33cbbfdefc738fdf86baf9a1d925b1
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-10 21:26:49 -0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -39,9 +39,11 @@ fn handle_header(response: &mut Response, header: &str) {
 fn get_header(request: &Request, header: &str) -> String {
     if request.headers.contains("\r\n") {
         let headers = request.headers.split("\r\n").collect::<Vec<&str>>();
-        headers.iter().find(|&x| x.contains(header)).unwrap().split(':').collect::<Vec<&str>>()[1].trim().to_string()
+        if let Some(h) = headers.iter().find(|&x| x.contains(header)) {
+            return h.split(':').collect::<Vec<&str>>()[1].trim().to_string();
+        }
     } else if request.headers.contains(header) {
-        request.headers.split(':').collect::<Vec<&str>>()[1].trim().to_string()
+        return request.headers.split(':').nth(1).map_or("".to_string(), |x| x.trim().to_string());
     } else {
         "".to_string()
     }
